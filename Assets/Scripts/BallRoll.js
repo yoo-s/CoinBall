1ae700f2f08366b81cae7468c90cb284362b6c13
yoo-s <soomin.yoo95@gmail.com>
2014-12-18 15:46:15 -0800

    Add unity project

---
+++ Assets/Scripts/BallRoll.js
@@ -0,0 +1,55 @@
+﻿#pragma strict
+
+/*function Start () {
+
+}*/
+
+var rotationSpeed = 600;
+var RotationSpeed = 600;
+var jumpHeight = 12;
+
+var hit01 : AudioClip;
+
+var distToGround : float;
+
+function Start () {
+	//Getting the distance from the center to the ground
+	distToGround = collider.bounds.extents.y;
+}
+
+function Update () {
+
+	//Handle rotation ball.
+	var rotation : float = Input.GetAxis ("Horizontal") * rotationSpeed;
+	var Rotation : float = Input.GetAxis ("Vertical") * RotationSpeed;
+	rotation *= Time.deltaTime;
+	Rotation *= Time.deltaTime;
+	rigidbody.AddForce(Vector3.right * rotation);
+	rigidbody.AddForce(Vector3.forward * Rotation);
+
+	if (Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
+		rigidbody.velocity.y = jumpHeight;
+	}
+}
+
+function IsGrounded () : boolean {
+	return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1);
+}
+
+//only horizontal movement
+/*	var rotation : float = Input.GetAxis ("Horizontal") * rotationSpeed;
+	rotation *= Time.deltaTime;
+	rigidbody.AddRelativeTorque (Vector3.back * rotation * 600);
+	
+	if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && isFalling == false) {
+		rigidbody.velocity.y = jumpHeight;
+	}
+	isFalling = true;
+}*/
+
+function OnCollisionEnter () {
+	audio.clip = hit01;
+	audio.volume = 1;
+	audio.pitch = Random.Range(0.9, 1.1);
+	audio.Play();
+}
---
+++ Assets/Scripts/GameMaster.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+static var currentScore: int = 0;
+
+var offsetY : float = 10;
+var sizeX : float = 100;
+var sizeY : float = 30;
+
+function Start () {
+	currentScore = 0;
+}
+
+function OnGUI () {
+	GUI.Box (new Rect (Screen.width - 120, offsetY, sizeX, sizeY), "Score: " + currentScore);
+}
